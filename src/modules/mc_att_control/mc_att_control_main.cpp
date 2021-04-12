91e6313647711131786669402ddb3040f059f48d
Jeremie Bannwarth <jban039@aucklanduni.ac.nz>
2021-04-12 14:38:31 +1200

    PHD-15 Implement manual horizontal thrust control

--- src/modules/mc_att_control/mc_att_control_main.cpp
+++ src/modules/mc_att_control/mc_att_control_main.cpp
@@ -753,10 +753,10 @@ MulticopterAttitudeControl::run()
 				_actuators.control[1] = (PX4_ISFINITE(_att_control(1))) ? _att_control(1) : 0.0f;
 				_actuators.control[2] = (PX4_ISFINITE(_att_control(2))) ? _att_control(2) : 0.0f;
 				_actuators.control[3] = (PX4_ISFINITE(_thrust_sp)) ? _thrust_sp : 0.0f;
-				/* Feedforward horizontal thrust */
-				_actuators.control[5] = (PX4_ISFINITE(_manual_control_sp.aux1)) ? _manual_control_sp.aux1 : 0.0f;
-				_actuators.control[7] = (PX4_ISFINITE(_manual_control_sp.aux2)) ? _manual_control_sp.aux2 : 0.0f;
-				//_actuators.control[7] = _v_att_sp.landing_gear;
+				/* Manual horizontal thrust - temporary */
+				_actuators.control[4] = (PX4_ISFINITE(_manual_control_sp.aux1)) ? _manual_control_sp.aux1 * 0.1f : 0.0f;
+				_actuators.control[5] = (PX4_ISFINITE(_manual_control_sp.aux2)) ? _manual_control_sp.aux2 * 0.1f : 0.0f;
+				_actuators.control[7] = _v_att_sp.landing_gear;
 				_actuators.timestamp = hrt_absolute_time();
 				_actuators.timestamp_sample = _sensor_gyro.timestamp;
 
